b74021c4519975fd8f5b7e4ebfbcf8e62bdb9805
Realnapsta <yuengbuc@gmail.com>
2020-06-26 07:19:11 +0000

    final commit

---
+++ XamWallet/XamWallet/Helpers/AzureConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace XamWallet.Helpers
+{
+    public class AzureConstants
+    {
+        public AzureConstants()
+        {
+        }
+
+        public const string FacialRecognitionAPIKey = "0e211f7f83e0449189d28782fda59c34";
+        //"Enter Your API Key";
+
+        //FaceApi example: https://westus.api.cognitive.microsoft.com/
+        public const string FaceApiBaseUrl = "https://xamwalletfaceapi.cognitiveservices.azure.com/";
+        // "Enter Your Face Api Base Url";
+    }
+}
---
+++ XamWallet/XamWallet/Views/Payments/VodafonePaymentPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace XamWallet.Views.Payments
+{
+    public partial class VodafonePaymentPage : PopupPage
+    {
+       
+
+        public VodafonePaymentPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+
+            this.IsBusy = false;
+        }
+
+        public void CancelBtn_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync(true);
+        }
+
+        public void submitBtn_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
